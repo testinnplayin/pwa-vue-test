76eddd9e45fe64b67ec0fc03029e492ec262ea72
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-05 13:57:17 +0200

    added a call for a single thingamabob

--- src/components/mixins/thingamabobAPI.js
+++ src/components/mixins/thingamabobAPI.js
@@ -7,6 +7,7 @@ export default {
         return {
             bAddress : `${apiAddress}/thingamabobs`,
             tBackMsg : null,
+            thingamabob : null,
             thingamabobs : null
         }
     },
@@ -28,6 +29,24 @@ export default {
                     this.tBackMsg = 'Successful retrieval of thingamabobs'
                 })
                 .catch(err => this.tBackMsg = `Error retrieving thingamabobs: ${err}`)
+        },
+        getThinggy : function (tId) {
+            const endpnt = `${this.bAddress}/${tId}`,
+                reqOpts = { method : 'GET', mode : 'cors' },
+                getReq = new Request(endpnt, reqOpts)
+            
+            fetch(getReq)
+                .then(response => {
+                    if (!response.ok) throw new Error(response.statusText)
+                    return response
+                })
+                .then(res => res.json()
+                .then(data => {
+                    console.log('data ', data)
+                    this.thingamabob = data.thingamabob
+                    this.tBackMsg = `Successful retrieval of thingamabob ${tId}`
+                }))
+                .catch(err => this.tBackMsg = `Error retrieving thingamabob ${tId}: ${err}`)
         }
     }
 }
