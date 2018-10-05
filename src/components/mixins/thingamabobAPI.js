7aaa7b9fa29f7fb4f9bbda4f1ae32573157a60e5
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-05 13:10:18 +0200

    added API call and a utils file

---
+++ src/components/mixins/thingamabobAPI.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import {apiAddress} from '../../utils'
+
+export default {
+    data : function () {
+        return {
+            bAddress : `${apiAddress}/thingamabobs`,
+            tBackMsg : null,
+            thingamabobs : null
+        }
+    },
+    methods : {
+        getThingamabobs : function () {
+            const endpnt = this.bAddress,
+                reqOpts = { method : 'GET', mode : 'cors' },
+                getReq = new Request(endpnt, reqOpts)
+
+            fetch(getReq)
+                .then(response => {
+                    if (!response.ok) throw new Error(response.statusText)
+
+                    return response
+                })
+                .then(res => res.json())
+                .then(data => {
+                    this.thingamabobs = data.thingamabobs
+                    this.tBackMsg = 'Successful retrieval of thingamabobs'
+                })
+                .catch(err => this.tBackMsg = `Error retrieving thingamabobs: ${err}`)
+        }
+    }
+}
--- src/views/Thingamabobs.vue
+++ src/views/Thingamabobs.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+  <div class="thingamabobs">
     <h1>This is a page about thingamabobs</h1>
   </div>
 </template>
+
+<script>
+import thingamabobAPI from '@/components/mixins/thingamabobAPI'
+
+export default {
+  name : 'Thingamabobs',
+  created : function () {
+    this.getThingamabobs()
+  },
+  mixins : [thingamabobAPI],
+}
+</script>
