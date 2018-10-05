8fcea989b16ad6d8f7936948def3c09a2354b741
Rachelle Wood <rachelle.wood@hypervision-technology.com>
2018-10-05 13:59:33 +0200

    creating a modal for thingamabobs

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,6 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width : 100%;
 }
 </style>
--- src/views/Thingamabobs.vue
+++ src/views/Thingamabobs.vue
@@ -1,17 +1,64 @@
 <template>
-  <div class="thingamabobs">
-    <h1>This is a page about thingamabobs</h1>
-  </div>
+  <main class="thingamabobs container">
+    <h1>Thingamabob List:</h1>
+    <b-row>
+      <b-col sm="12">
+
+        <b-list-group v-if="thingamabobs && thingamabobs.length > 0"
+          class="t-list">
+          <b-list-group-item v-for="(thingamabob, ind) of thingamabobs"
+            @click="handleTClick"
+            :id="thingamabob._id"
+            :key="ind"
+            button
+            class="t-list-item">{{thingamabob.awesome_field}}</b-list-group-item>
+        </b-list-group>
+
+        <section v-else>
+          <h2>Loading...</h2>
+        </section>
+
+      </b-col>
+    </b-row>
+    <t-modal></t-modal>
+  </main>
 </template>
 
 <script>
+// Mixins
 import thingamabobAPI from '@/components/mixins/thingamabobAPI'
+
+// Components
+import TModal from '@/components/TModal'
 
 export default {
   name : 'Thingamabobs',
+  data : function () {
+    return {
+      showModal : false
+    }
+  },
+  components : {TModal},
   created : function () {
     this.getThingamabobs()
+  },
+  methods : {
+    handleTClick : function (e) {
+      console.log('handleTClick ', e.currentTarget)
+      this.getThinggy(e.currentTarget.getAttribute('id'));
+      this.showModal = true
+    }
   },
   mixins : [thingamabobAPI],
 }
 </script>
+
+<style>
+.thingamabobs {
+  margin-top: 15px;
+  width: 100%;
+}
+.t-list {
+  margin-top: 20px;
+}
+</style>
